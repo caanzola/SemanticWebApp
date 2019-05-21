7559e1f5ff823f869631c5ce8fddd9d0a2662b76
Diana Marcela <dm.gutierrez11@uniandes.edu.co>
2019-05-20 23:19:00 -0500

    arreglos

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -16,15 +16,23 @@ export default class Navbar extends Component {
   render() {
     return (
       <div className='fixed-top'>
-        <nav id='navbar_home' className='navbar navbar-dark bg-dark'>
+        <nav id='navbar_home' className='navbar navbar-expand-lg navbar-light bg-light'>
           <a id='nombre_nav' className='navbar-brand' 
              href={'#'}>
-             Semantic Web
+             Academic papers
           </a>
-          <a className='nav-item nav-link ' onClick={this.state.onChange.bind(this, 'Instancias')} href={'#instancias'}>Instancias</a>
-          <a className='nav-item nav-link ' onClick={this.state.onChange.bind(this, 'Clases')} href={'#clases'}>Clases</a>
-          <a className='nav-item nav-link ' onClick={this.state.onChange.bind(this, 'Propiedades')} href={'#propiedades'}>Propiedades</a>
-          <a className='nav-item nav-link' onClick={this.state.onChange.bind(this, 'Viz')} href={'#viz'}>Graphs</a>
+          <ul class="navbar-nav">
+      <li class="nav-item active">
+      <a className='nav-item nav-link ' onClick={this.state.onChange.bind(this, 'Instancias')} href={'#instancias'}>Instancias</a>
+      </li>
+      <li class="nav-item">
+      <a className='nav-item nav-link ' onClick={this.state.onChange.bind(this, 'Clases')} href={'#clases'}>Clases</a>
+      </li>
+      <li class="nav-item">
+      <a className='nav-item nav-link ' onClick={this.state.onChange.bind(this, 'Propiedades')} href={'#propiedades'}>Propiedades</a>
+      </li>
+    </ul>
+          
         </nav>
       
       </div>
